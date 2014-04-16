fc74077bd589bec4b0e813c0571f15817bc76418
sufendyc <sufendy.chai@my.experian.com>
2014-04-16 22:54:40 +0800

    Added average time scoring in both single game and main scorer

--- src/battleships/scorer.py
+++ src/battleships/scorer.py
@@ -5,7 +5,7 @@ from battleships.data.bots import BotsDataSync as BotsData
 from battleships.data.users import UsersDataSync as UsersData
 from battleships.engine.battleships2 import BattleshipsGame
 from battleships.player import Player
-
+import time
 
 class Scorer(object):
     """Score a bot by playing it against a number of games and taking the
@@ -25,17 +25,22 @@ class Scorer(object):
             # run games
             runner_game = Player(BattleshipsGame)
             scores = []
+            times = []
             for i in range(num_games):
+                start_time = time.time()
                 game_result = runner_game.play(bot_path)
                 if not game_result["success"]:
                     raise _ScoringException(game_result)
+
+                times.append(time.time() - start_time)
                 scores.append(game_result["score"])
                 cls._log.info("%s played %s/%s" % (bot_path, i+1, num_games))
 
             # update database with success
             score = sum(scores) / float(num_games) # average
-            BotsData.score_success(bot_id, score)
-            UsersData.set_state_to_scored_success(user_id, bot_id, score)
+            avg_time = sum(times) / float(num_games)
+            BotsData.score_success(bot_id, score, avg_time)
+            UsersData.set_state_to_scored_success(user_id, bot_id, score, avg_time)
 
         except _ScoringException as e:
             cls._log.warning("%s scoring aborted: %s" % \
